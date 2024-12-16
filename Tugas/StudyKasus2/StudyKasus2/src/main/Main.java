bd97aaa061f2d076f946acbeaed6849d703a583d
Diazalfiari <diazalfiari19@gmail.com>
2024-12-16 07:56:28 +0700

    Mengerjakan Study kasus 2

---
+++ Tugas/StudyKasus2/StudyKasus2/src/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import model.MyBatisUtil; 
+import org.apache.ibatis.session.SqlSession; 
+import view.ProductsView;
+import controller.ProductsController;
+import model.ProductMapper;
+/**
+ *
+ * @author Diaza
+ */
+public class Main {
+    public static void main(String[] args) {
+        SqlSession session = MyBatisUtil.getSqlSession(); 
+        ProductMapper mapper = session.getMapper(ProductMapper.class);
+        
+        ProductsView view = new ProductsView(); 
+        new ProductsController(view, mapper);
+        
+        view.setVisible(true);
+    }
+}
+
